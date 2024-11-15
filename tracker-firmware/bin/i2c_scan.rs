212c05c839684421527c1e247dc3ed165cb0cea3
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2024-11-15 22:27:05 +0100

    Vastly improve i2c scanner

--- tracker-firmware/bin/i2c_scan.rs
+++ tracker-firmware/bin/i2c_scan.rs
@@ -1,18 +1,29 @@
 #![no_std]
 #![no_main]
 
-use defmt::println;
+use defmt::{info, println, trace, Format};
 use embassy_executor::Spawner;
-use embassy_rp::{
-    bind_interrupts, config,
-    i2c::{self, InterruptHandler as I2cInterruptHandler},
-    peripherals::I2C0,
-};
+use embassy_futures::select::Either;
+use embassy_rp::{bind_interrupts, config, i2c, peripherals::I2C0};
+use embassy_time::{Delay, Duration, Ticker};
+use embedded_hal_async::{delay::DelayNs, i2c::I2c};
+use heapless::Vec;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    I2C0_IRQ => I2cInterruptHandler<I2C0>;
+    I2C0_IRQ => i2c::InterruptHandler<I2C0>;
 });
+
+const LOOP_DURATION: Duration = Duration::from_millis(1000);
+
+#[derive(Debug, Format, thiserror::Error)]
+pub enum Error {
+    #[error("I2C operation timed out")]
+    Timeout,
+
+    #[error("I2C operation failed")]
+    I2cError(i2c::Error),
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
@@ -23,16 +34,45 @@ async fn main(_spawner: Spawner) -> ! {
 
     let mut i2c = i2c::I2c::new_async(p.I2C0, scl, sda, Irqs, i2c::Config::default());
 
-    for addr in 1..=127 {
-        println!("Scanning Address {}", addr as u8);
+    let mut ticker = Ticker::every(LOOP_DURATION);
 
-        // Scan Address
-        let res = i2c.blocking_read(addr as u8, &mut [0]);
+    let mut delay = Delay;
 
-        // Check and Print Result
-        if res.is_ok() {
-            println!("Device Found at Address {}", addr as u8);
+    let mut devices: Vec<u8, 127> = heapless::Vec::new();
+    loop {
+        let mut changed = false;
+        for addr in 1..=127 {
+            trace!("Scanning Address {}", addr as u8);
+
+            let timeout = delay.delay_ms(10);
+            let result = embassy_futures::select::select(timeout, i2c.read(addr, &mut [0])).await;
+
+            let result = match result {
+                Either::Second(Ok(())) => Ok(()),
+                Either::Second(Err(e)) => Err(Error::I2cError(e)),
+                Either::First(()) => Err(Error::Timeout),
+            };
+
+            let present = result.is_ok();
+            let known = devices.contains(&addr);
+            match (known, present) {
+                (true, true) => trace!("    0x{:x} known device, unchanged", addr),
+                (false, false) => trace!("No device on 0x{:x}", addr),
+                (true, false) => {
+                    devices.retain(|elem| *elem != addr);
+                    changed = true;
+                    info!("    Device at address 0x{:x} disconnected", addr);
+                }
+                (false, true) => {
+                    devices.push(addr).unwrap();
+                    changed = true;
+                    info!("    Device at address 0x{:x} found", addr as u8);
+                }
+            }
         }
+        if !devices.is_empty() && changed {
+            println!("Devices: {=[u8]:#x}", devices);
+        }
+        ticker.next().await;
     }
-    panic!();
 }
